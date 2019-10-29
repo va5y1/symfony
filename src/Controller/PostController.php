498c5ee842750bd6787555f566bd9448b5c41820
Cable <v.kydyba@drudesk.com>
2019-10-29 14:28:58 +0200

    migration

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -12,8 +12,22 @@ class PostController extends AbstractController
      */
     public function index()
     {
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+      $posts = [
+        'post1' => [
+          'title' => '1 Заголовок',
+          'body' => 'Тело первого поста'
+        ],
+        'post2' => [
+          'title' => '2 Заголовок',
+          'body' => 'Тело второго постdfgdfgsdfgsdfgsdgdgdfgsggdfgsdfgsdfg sdfg sdf g sd sdf  dfg sdfg sd sdf gfs sdf sdf sdf sdfg  а'
+        ],
+        'post3' => [
+          'title' => '3 Заголовок',
+          'body' => 'Тело третьего поста'
+        ],
+      ];
+      return $this->render('post/index.html.twig', [
+            'posts' => $posts
         ]);
     }
 }
